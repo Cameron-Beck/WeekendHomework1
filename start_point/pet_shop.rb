d3ecc8e88d588dc110ac27eed095c045084ea91d
Cameron <cameron.beckit@gmail.com>
2019-10-21 10:33:20 +0100

    final submission

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -17,8 +17,19 @@ end
 
 def increase_pets_sold(sold, amount)
   sold = (@pet_shop[:admin][:pets_sold] = amount)
+  return sold
 end
 
 def stock_count(count)
   return @pet_shop[:pets].count
 end
+
+def pets_by_breed(pets, breed)
+  counter = 0
+  
+end
+
+# def find_pet_by_name(pet, name)
+#   names = @pet_shop[:pets][:name]
+#   p names
+# end
